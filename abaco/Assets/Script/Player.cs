1087f69eeac3b2fd7342c9c564abe4d8c546933b
dede2008w <tomypeekblinders@gmail.com>
2024-08-05 10:24:03 -0300

    silvio santos comitou
    bah roe

--- abaco/Assets/Script/Player.cs
+++ abaco/Assets/Script/Player.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Rigidbody2D rigidbody2D;
+    float direction;
+    float speed = 20; 
+    
+   void Move()
     {
-        
+        rigidbody2D.velocity =  new Vector2 (direction * speed, 0);
+     if(transform.position.x < 0) 
+        {
+            transform.position = new Vector2(-GameManager.instance.ScreenBounds.x,transform.position.y);
+        }
+    }
+    private void Update()
+    {
+        direction = Input.GetAxis("Horizontal");
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
+
 }

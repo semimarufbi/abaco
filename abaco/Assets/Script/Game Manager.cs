f3b582921f441665ebefe5455b5c33e0ca7a379b
RafaelRaiser <rafabarrosraiser@gmail.com>
2024-08-05 08:38:06 -0300

    Initial Commit GameManager

---
+++ abaco/Assets/Script/Game Manager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+  static GameManager instance;
+    Vector2 screenBounds;
+    int score;
+    int lifes = 3;
+
+    public Vector2 ScreenBounds { get => screenBounds;  }
+    public int Score { get => score; set => score = value; }
+    public int Lifes { get => lifes; }
+
+    private void Awake()
+    {
+         instance = this;
+
+
+    }
+}

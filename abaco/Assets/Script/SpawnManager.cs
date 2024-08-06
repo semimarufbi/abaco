29e49755cea23ef448627bf294e85770459771cc
RafaelRaiser <rafabarrosraiser@gmail.com>
2024-08-06 11:18:46 -0300

    Fix-GameManage#2r

--- abaco/Assets/Script/SpawnManager.cs
+++ abaco/Assets/Script/SpawnManager.cs
@@ -4,27 +4,6 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    [SerializeField]
-    GameObject redApple;
-    [SerializeField]
-    GameObject greenApple;
-    [SerializeField]
-    GameObject goldenApple;
-
-    float timer, initialTime = 1.2f;
-    int appleType;
-
-    void Spawn() 
-    { 
-        if (timer <= 0 ) 
-        {
-            appleType = Random.Range(0, 100);
-        }
-        else if (appleType > 90) 
-        { 
-            
-=======
     [SerializeField]  GameObject redApple;
     [SerializeField]  GameObject greenApple;
     [SerializeField]  GameObject goldenApple;
@@ -82,7 +61,6 @@ public class SpawnManager : MonoBehaviour
         else
         {
             timer -= Time.deltaTime;
->>>>>>> Stashed changes
         }
     }
 }
